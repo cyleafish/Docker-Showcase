595095ae5496a6df0f87ff365ec1293603a7b393
leafish <155407634+cyleafish@users.noreply.github.com>
2025-05-10 02:18:03 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   port:'3306',
-//   user: 'root',
-//   password: 'root',
-//   database: 'judge_questions'
-// }); 
 const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER ,
